aeb10fca7a1e0b20779beea6a126abc3905698b9
Jawad Muhammadi <jawad10@Jawads-MacBook-Pro.local>
2018-10-25 16:38:34 -0700

    added seed for database

--- database/seed.js
+++ database/seed.js
@@ -1,31 +1,19 @@
-// const faker = require('faker');
-// const fs = require('fs');
 
-// faker.seed(105);
-
-// const fakeRestaurants = [];
-
-// for (let i = 1; i <= 100; i++) {
-//   const fakeRestaurant = {id: i};
-//   const fakeWords = []
-//   const length = faker.random.number() % 3;
-
-//   for (let i = 0; i <= length; i++) {
-//     fakeWords.push(faker.random.word())
-//   }
-
-//   fakeRestaurant.name = fakeWords.join(' ');
-
-//   fakeRestaurants.push(fakeRestaurant);
-// }
-
-// const restaurantsJSON = JSON.stringify(fakeRestaurants);
-
-// fs.writeFileSync('fakerestaurants.json', restaurantsJSON);
 
 const faker = require('faker');
 const fs = require('fs');
 const Reservation = require('./Reservation');
+var fakeData = [];
+for (var i = 0; i <= 100; i++) {
+  const User = {
+    id: i,
+    size: faker.random.number(),
+    date: faker.date.recent(),
+    time: faker.date.soon
+  };
+  fakeData.push(User);
+}
 
 
-Reservation.create(faker(100));
+
+Reservation.create(fakeData);

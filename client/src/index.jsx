1ab439900f745008b60c2e622321a840cdfb7199
Jawad Muhammadi <jawad10@Jawads-MacBook-Pro.local>
2018-10-30 13:41:49 -0700

    fixed main component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Nav from './components/Nav.jsx';
-import Images from './components/Images.jsx';
-import MainBody from './components/MainBody.jsx';
+import App from './components/App.jsx';
 
-// class App extends React.Component {
-//     constructor(props) {
-//         super(props);
-//     }
 
-//     render() {
-//         return (
-//             <Nav />
-//         )
-//     }
-// }
 
-ReactDOM.render(<Nav />, document.getElementById('nav'));
+ReactDOM.render(<App />, document.getElementById('nav'));
 // ReactDOM.render(<Images />, document.getElementById('images'));
 // ReactDOM.render(<MainBody />, document.getElementById('main-body'));

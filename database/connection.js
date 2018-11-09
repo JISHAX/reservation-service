dc0031e0566926e5fc8452c19bfb6739411008b5
Jawad Muhammadi <muhammadijawad10@gmail.com>
2018-11-08 19:28:15 -0800

    dockerized module

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/reservations';
+const mongoUri = 'mongodb://database:27017/reservations';
 
 mongoose.connect(mongoUri, { useNewUrlParser: true});
 
